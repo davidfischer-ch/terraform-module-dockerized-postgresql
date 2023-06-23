302304ce75d7efb38ae01b77a64c49c120cc73f5
David Fischer <david@fisch3r.net>
2023-06-23 07:03:43 +0200

    Drop unused providers

--- versions.tf
+++ versions.tf
@@ -6,20 +6,5 @@ terraform {
       source  = "kreuzwerker/docker"
       version = ">= 3.0.2"
     }
-
-    jinja = {
-      source  = "NikolaLohinski/jinja"
-      version = ">= 1.15.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.4.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.2.1"
-    }
   }
 }
